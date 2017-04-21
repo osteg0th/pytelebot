444bd045347739cdbf61f4e3b0afeb3d6a72bc23
Vladislaus Rex <zxyghostyxz@gmail.com>
2017-04-21 23:04:00 +0300

    make it better

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def webhook():
     return "CONNECTED", 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 8000))
